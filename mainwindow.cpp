adcad9a434b70ea7fbfdf1791af5a4d89a1dd1d0
IseanB <Isean4B@gmail.com>
2022-06-22 17:11:18 -0700

    Merged Battery and Warning Labels

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -183,3 +183,146 @@ void MainWindow::on_motorWarning_OFF_clicked()
     ui->warnLabelMotor->setPixmap(warnMotor_OFF);
 }
 
+void MainWindow::on_IncreaseMPH_clicked()
+{
+    if(mph < 100){
+        mph++;
+        ui->MPH->display(mph);
+    }
+}
+
+
+void MainWindow::on_DecreaseMPH_clicked()
+{
+    if(mph > 0){
+        mph--;
+        ui->MPH->display(mph);
+    }
+}
+
+
+void MainWindow::on_ResetMPH_clicked()
+{
+    mph = 0;
+    ui->MPH->display(mph);
+}
+
+
+void MainWindow::on_TurnRightBlinker_clicked()
+{
+   ui->rightBlinker->setPixmap(QPixmap(rightBlinkPath_ON));
+
+}
+
+
+void MainWindow::on_TurOffBlinkers_clicked()
+{
+   ui->rightBlinker->setPixmap(QPixmap(rightBlinkPath_OFF));
+}
+
+
+
+void MainWindow::on_TurnOffLeftBlinker_clicked()
+{
+    ui->LeftBlinker->setPixmap(QPixmap(leftBlinkPath_ON));
+}
+
+
+void MainWindow::on_LeftBlinker_2_clicked()
+{
+    ui->LeftBlinker->setPixmap(QPixmap(leftBlinkPath_OFF));
+}
+
+
+
+void MainWindow::on_decreaseBattery_clicked()
+{
+    if(percentage > 0){
+       percentage--;
+    }
+
+    ui->batteryPercent->setNum(percentage);
+    if(height1 > 0 && percentage < 99){
+        height1--;
+        verticalShift1++;
+        ui->batteryHeight1->setFixedHeight(height1);
+        ui->batteryHeight1->setGeometry(670, 300+verticalShift1, 71, height1);
+    }
+    else if(height1 == 0 && height2 > 0){
+        height2--;
+        verticalShift2++;
+        ui->batteryHeight2->setFixedHeight(height2);
+        ui->batteryHeight2->setGeometry(670, 340+verticalShift2, 71, height2);
+    }
+    else if(height2 == 0 && height3 > 0){
+        height3--;
+        verticalShift3++;
+        ui->batteryHeight3->setFixedHeight(height3);
+        ui->batteryHeight3->setGeometry(670, 380+verticalShift3, 71, height3);
+    }
+    if(percentage <= 20){
+        ui->lowBatteryWarning->setPixmap(QPixmap("C:/Users/iseanbhanot/Documents/SolarCar/mergeFold2/Driver-Interface-2022/icons8-battery-alert-48.png"));
+    }
+    else{
+        ui->lowBatteryWarning->setPixmap(QPixmap("blank"));
+    }
+
+}
+
+
+
+
+void MainWindow::on_resetBattery_clicked()
+{
+    percentage = 100;
+    ui->batteryPercent->setNum(percentage);
+    height1 = 33;
+    verticalShift1 = 0;
+    ui->batteryHeight1->setFixedHeight(height1);
+    ui->batteryHeight1->setGeometry(670, 300, 71, height1);
+
+    height2 = 33;
+    verticalShift2 = 0;
+    ui->batteryHeight2->setFixedHeight(height2);
+    ui->batteryHeight2->setGeometry(670, 340, 71, height2);
+
+    height3 = 33;
+    verticalShift3 = 0;
+     ui->batteryHeight3->setFixedHeight(height3);
+    ui->batteryHeight3->setGeometry(670, 380, 71, height3);
+}
+
+
+void MainWindow::on_increaseBattery_clicked()
+{
+    if(percentage < 100){
+       percentage++;
+    }
+
+    ui->batteryPercent->setNum(percentage);
+    if(height3 < 33){
+        height3++;
+        verticalShift3--;
+        ui->batteryHeight3->setFixedHeight(height3);
+        ui->batteryHeight3->setGeometry(670, 380+verticalShift3, 71, height3);
+    }
+    else if(height3 == 33 && height2 < 33){
+        height2++;
+        verticalShift2--;
+        ui->batteryHeight2->setFixedHeight(height2);
+        ui->batteryHeight2->setGeometry(670, 340+verticalShift2, 71, height2);
+    }
+    else if(height2 == 33 && height1 < 33){
+        height1++;
+        verticalShift1--;
+        ui->batteryHeight1->setFixedHeight(height1);
+        ui->batteryHeight1->setGeometry(670, 300+verticalShift1, 71, height1);
+    }
+    if(percentage <= 20){
+        ui->lowBatteryWarning->setPixmap(QPixmap("C:/Users/justiny/Downloads/icons8-battery-alert-48.png"));
+    }
+    else{
+        ui->lowBatteryWarning->setPixmap(QPixmap("blank"));
+    }
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -38,6 +38,27 @@ private slots:
 
     void on_motorWarning_OFF_clicked();
 
+
+    void on_IncreaseMPH_clicked();
+
+    void on_DecreaseMPH_clicked();
+
+    void on_ResetMPH_clicked();
+
+    void on_TurnRightBlinker_clicked();
+
+    void on_TurOffBlinkers_clicked();
+
+    void on_TurnOffLeftBlinker_clicked();
+
+    void on_LeftBlinker_2_clicked();
+
+    void on_decreaseBattery_clicked();
+
+    void on_resetBattery_clicked();
+
+    void on_increaseBattery_clicked();
+
 private:
     Ui::MainWindow *ui;
     QPixmap warnBattery_ON;  //Stores image of warning label when ON  and (x,y) where its placed
@@ -46,6 +67,18 @@ private:
     QPixmap warnSolar_OFF; //Stores image of warning label when OFF and (x,y) where its placed
     QPixmap warnMotor_ON;  //Stores image of warning label when ON  and (x,y) where its placed
     QPixmap warnMotor_OFF; //Stores image of warning label when OFF and (x,y) where its placed
+    int mph = 0;
+    int percentage = 100;
+    int height1 = 33;
+    int verticalShift1 = 0;
+    int height2 = 33;
+    int verticalShift2 = 0;
+    int height3 = 33;
+    int verticalShift3 = 0;
+    const char* leftBlinkPath_ON = "/Users/iseanbhanot/Documents/SolarCar/another1/Driver-Interface-2022/leftFull.png";
+    const char* leftBlinkPath_OFF = "/Users/iseanbhanot/Documents/SolarCar/another1/Driver-Interface-2022/leftEmpty.png";
+    const char* rightBlinkPath_ON = "/Users/iseanbhanot/Documents/SolarCar/another1/Driver-Interface-2022/rightFull.png";
+    const char* rightBlinkPath_OFF = "/Users/iseanbhanot/Documents/SolarCar/another1/Driver-Interface-2022/rightEmpty.png";
 };
 
 
