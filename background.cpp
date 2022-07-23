3ca9b0e0b9b9dcd00b2644fec415bc556a32bd4c
Val <vwong031@ucr.edu>
2022-07-22 22:24:39 -0700

    Dark style theme

--- background.cpp
+++ background.cpp
@@ -2,12 +2,13 @@
 
 void Background::darkTheme() {
   QFile f( ":qdarkstyle/style.qss" );
-  
+
   if ( !f.exists() ) {
     qWarning() << "Unable to set dark stylesheet, file not found";
   } else {
     f.open( QFile::ReadOnly | QFile::Text );
     QTextStream ts( &f );
-    getApp()->setStyleSheet( ts.readAll() );
+    qApp->setStyleSheet(ts.readAll());
+    // getApp()->setStyleSheet( ts.readAll() );
   }
 }
