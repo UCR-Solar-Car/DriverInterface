63ad73fb3d934e01707820874c39251da58d1a6d
Val <vwong031@ucr.edu>
2022-08-06 22:54:43 -0700

    Added low battery image & fix format

--- warnings.cpp
+++ warnings.cpp
@@ -4,19 +4,19 @@ Warnings::Warnings(){ return; }
 
 void Warnings::setup(Ui::MainWindow *ui) {
   this->ui = ui;
-    WarningLabel* battery = new WarningLabel(OFF,QPixmap(":/icons/battery.png"),ui->batteryWarning);
-    WarningLabel* motor= new WarningLabel(OFF,QPixmap(":/icons/motor.png"),ui->motorWarning);
+  WarningLabel* battery = new WarningLabel(OFF,QPixmap(":/icons/battery.png"),ui->batteryWarning);
+  WarningLabel* motor= new WarningLabel(OFF,QPixmap(":/icons/motor.png"),ui->motorWarning);
 
-    warning_labels.insert({BATTERY, battery});
-    warning_labels.insert({MOTOR, motor});
+  warning_labels.insert({BATTERY, battery});
+  warning_labels.insert({MOTOR, motor});
 }
 
 void Warnings::on(warnings warning){
-    warning_labels[warning]->state = ON;
-    warning_labels[warning]->label->setPixmap(warning_labels[warning]->image);
+  warning_labels[warning]->state = ON;
+  warning_labels[warning]->label->setPixmap(warning_labels[warning]->image);
 }
 
 void Warnings::off(warnings warning){
-    warning_labels[warning]->state = OFF;
-    warning_labels[warning]->label->setText("OFF");
+  warning_labels[warning]->state = OFF;
+  warning_labels[warning]->label->setText("OFF");
 }
