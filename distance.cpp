a7021ae42e2b54a33badcf57f50ba21033e4abc3
IseanB <Isean4B@gmail.com>
2022-08-10 22:59:42 -0700

    Added Scaling
    distance scaling

--- distance.cpp
+++ distance.cpp
@@ -1,4 +1,5 @@
 #include "distance.h"
+#include "scaler.h"
 
 Distance::Distance() : distance(0)
 {
@@ -8,6 +9,12 @@ Distance::Distance() : distance(0)
 void Distance::setup(Ui::MainWindow* ui) {
     this->ui = ui;
     ui->distance->display(distance);
+    int distanceSize = screenWidth*.05;
+
+    ui->distanceLabel->resize(distanceSize, distanceSize);
+    ui->distanceLabel->move(screenWidth/5, screenHeight*.45);
+    ui->distance->resize(screenWidth/6,distanceSize*1.2);
+    ui->distance->move(screenWidth/5, screenHeight*.45);
 }
 
 void Distance::decrease_distance(int value) {
