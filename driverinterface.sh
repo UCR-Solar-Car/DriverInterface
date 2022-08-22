b2487ef6b7495daf1e774929104adea23b9549b4
Ricardo Galeano <ricardogaleano@Ricardos-Air.home>
2022-08-22 16:56:32 -0700

    Restored original code and added clang-format script.

--- driverinterface.sh
+++ driverinterface.sh
@@ -1,8 +1,22 @@
 #!/bin/sh 
 
+cd /home/ucrsolarcar/UCRSolarCar2023/Driver-Interface-2022
+
 for FILE in *; do
   if [[ (${FILE: -4} == ".cpp") || (${FILE: -2} == ".h") ]]; then 
     echo "$FILE is being formatted..." 
     clang-format -i -style=Google ./$FILE
   fi
 done
+
+res=$(git pull origin main | grep Already)
+
+if [[ ${res} =~ 'Already' ]]
+then
+  echo 'No updates have been made :)'
+else
+  qmake DriverInterface.pro
+  make
+fi
+
+./DriverInterface
