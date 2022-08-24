11ac308bde5f23dc2fa50e7650f60e2e93f6b97c
Ricardo Galeano <ricardogaleano@Ricardos-Air.home>
2022-08-24 14:30:13 -0700

    Put formatting code in proper position.

--- driverinterface.sh
+++ driverinterface.sh
@@ -2,19 +2,18 @@
 
 cd /home/ucrsolarcar/UCRSolarCar2023/Driver-Interface-2022
 
-for FILE in *; do
-  if [[ (${FILE: -4} == ".cpp") || (${FILE: -2} == ".h") ]]; then 
-    echo "$FILE is being formatted..." 
-    clang-format -i -style=Google ./$FILE
-  fi
-done
-
 res=$(git pull origin main | grep Already)
 
 if [[ ${res} =~ 'Already' ]]
 then
   echo 'No updates have been made :)'
 else
+  for FILE in *; do
+  if [[ (${FILE: -4} == ".cpp") || (${FILE: -2} == ".h") ]]; then 
+    echo "$FILE is being formatted..." 
+    clang-format -i -style=Google ./$FILE
+  fi
+done
   qmake DriverInterface.pro
   make
 fi
