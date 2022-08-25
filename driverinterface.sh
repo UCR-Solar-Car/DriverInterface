5c1d1eca81cfd2492173f1ad4eb07dd0298d848c
Divyank Shah <divyank.shah.2016@gmail.com>
2022-08-25 15:00:12 -0700

    formatting

--- driverinterface.sh
+++ driverinterface.sh
@@ -6,14 +6,14 @@ res=$(git pull origin main | grep Already)
 
 if [[ ${res} =~ 'Already' ]]
 then
-  echo 'No updates have been made :)'
+    echo 'No updates have been made :)'
 else
   for FILE in *; do
-  if [[ (${FILE: -4} == ".cpp") || (${FILE: -2} == ".h") ]]; then 
-    echo "$FILE is being formatted..." 
-    clang-format -i -style=Google ./$FILE
-  fi
-done
+    if [[ (${FILE: -4} == ".cpp") || (${FILE: -2} == ".h") ]]; then 
+      echo "$FILE is being formatted..." 
+      clang-format -i -style=Google ./$FILE
+    fi
+  done
   qmake DriverInterface.pro
   make
 fi
