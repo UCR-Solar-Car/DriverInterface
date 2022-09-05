4b0ffde9c1c388c8a17d6474488f81d83c4c67dc
Divyank Shah <divyank.shah.2016@gmail.com>
2022-09-04 21:08:14 -0700

    Update driverinterface.sh

--- driverinterface.sh
+++ driverinterface.sh
@@ -8,11 +8,6 @@ if [[ ${res} =~ 'Already' ]]
 then
     echo 'No updates have been made :)'
 else
-  for FILE in *; do
-    if [[ (${FILE: -4} == ".cpp") || (${FILE: -2} == ".h") ]]; then 
-      echo "$FILE is being formatted..." 
-      clang-format -i -style=Google ./$FILE
-    fi
   done
   qmake DriverInterface.pro
   make
